a9f3a01a906a8d8c3e4f13368824cb9b46fe54c9
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-06-08 16:24:59 +0300

    Header styled component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { StyledHeader } from "./styles/Header.styled";
 
 export default function Header() {
   return (
-    <header>
+    <StyledHeader>
       <h1>Header</h1>
-    </header>
+    </StyledHeader>
   );
 }
