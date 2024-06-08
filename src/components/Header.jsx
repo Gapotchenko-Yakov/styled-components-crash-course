0ada72e19388f42d3b82f71203f69fefe6d01838
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-06-08 19:28:52 +0300

    Made Header and all necessary styled components. Fix Container max-width typo.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { StyledHeader } from "./styles/Header.styled";
+import { Logo, Nav, StyledHeader } from "./styles/Header.styled";
 import { Container } from "./styles/Container.styled";
+import { Button } from "./styles/Button.styled";
 
 export default function Header() {
   return (
     <StyledHeader>
       <Container>
-        <h1>Huddle</h1>
+        <Nav>
+          <Logo src="./images/logo.svg" alt="" />
+          <Button>Try It Free</Button>
+        </Nav>
       </Container>
     </StyledHeader>
   );
